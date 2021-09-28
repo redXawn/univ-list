185ff874a1425397c98d622c192b055af03bde35
Sebastian Andrew <sebastian.andre@ovo.id>
2021-09-28 11:41:20 +0700

    add skeleton loading in homepage

--- src/view/homepage.js
+++ src/view/homepage.js
@@ -1,19 +1,22 @@
-import React, { useEffect, lazy, Suspense } from "react";
+import React, { useEffect, lazy, Suspense, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import Loading from "../components/loading";
+// import Skeleton from "../components/skeleton";
+
 import infiniteScrolling from "../utils/infiniteScroll";
 import { getUniversities, incrementPagination } from "../redux/action/universitiesAction";
 
 import "./homepage.scss";
 
 const Card = lazy(() => import("../components/card"));
+const Skeleton = lazy(() => import("../components/skeleton"));
 
 const Homepage = (props) => {
   infiniteScrolling(getMoreUnivList);
 
   const dispatch = useDispatch();
-  const loading = useSelector((state) => state.loadingReducer);
+  const loading = useSelector((state) => state.loadingReducer.loading);
   const universitiesReducer = useSelector((state) => state.universitiesReducer);
   const { univName, univCountry, universitiesList, paginationList, currentPage, totalPage } = universitiesReducer;
 
@@ -40,8 +43,30 @@ const Homepage = (props) => {
           </a>
         </Card>
       ));
-    } else {
-      return <div>empty</div>;
+    }
+  }
+
+  function renderSkeletonCard() {
+    if (loading) {
+      return (
+        <Fragment>
+          <Card customClassName="j-content--center">
+            <Skeleton height={40} width={140} />
+            <Skeleton height={25} width={150} />
+            <Skeleton height={25} width={200} />
+          </Card>
+          <Card customClassName="j-content--center">
+            <Skeleton height={40} width={140} />
+            <Skeleton height={25} width={150} />
+            <Skeleton height={25} width={200} />
+          </Card>
+          <Card customClassName="j-content--center">
+            <Skeleton height={40} width={140} />
+            <Skeleton height={25} width={150} />
+            <Skeleton height={25} width={200} />
+          </Card>
+        </Fragment>
+      );
     }
   }
 
@@ -58,6 +83,7 @@ const Homepage = (props) => {
         <label>Country: {univCountry}</label>
       </div>
       <div className="homepage-card-wrapper">{renderUnivCard()}</div>
+      <div className="homepage-card-wrapper">{renderSkeletonCard()}</div>
     </Suspense>
   );
 };
